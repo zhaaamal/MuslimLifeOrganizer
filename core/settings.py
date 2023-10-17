7b123fb0390410cedf571abecb030aab21d044fe
zhaaamal <madiarovanurjamal@gmail.com>
2023-10-17 11:57:43 +0600

    replace sql to psql

--- core/settings.py
+++ core/settings.py
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mlo',  # Имя вашей базы данных
+        'USER': 'myuser',      # Имя вашего пользователя
+        'PASSWORD': 'mypass',  # Ваш пароль
+        'HOST': 'localhost',   # Хост, на котором работает PostgreSQL
+        'PORT': '5432',            # Порт (по умолчанию 5432)
     }
 }
 
